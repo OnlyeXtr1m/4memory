6590f8b8022d4af3d510425c311b10aae1bbfa4f
Anton.Prokharchyk <Anton.Prokharchyk@gmail.com>
2023-09-26 18:40:36 +0300

    fix button props types

--- src/components/button/button.proptypes.ts
+++ src/components/button/button.proptypes.ts
@@ -3,5 +3,4 @@ import {ButtonHTMLAttributes, DetailedHTMLProps, ReactNode} from "react";
 export interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
   children: ReactNode;
   displayType: "primary" | "dark" | "bordered" | "none";
-  props?: ButtonHTMLAttributes<HTMLButtonElement>;
 }

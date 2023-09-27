d0220b3288c3daffd954fac1373703160c16aaac
Anton.Prokharchyk <Anton.Prokharchyk@gmail.com>
2023-09-27 18:20:10 +0300

    added eslint-prettier-husky

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import cn from "classnames";
+import cn from 'classnames';
 
-import {ButtonProps} from "@/components/button/button.proptypes";
+import { ButtonProps } from '@/components/button/button.proptypes';
 
-import styles from "./button.module.scss";
-export function Button({children, buttonType = "primary", ...props}: ButtonProps) : React.ReactElement {
-  return <button className={cn(styles.btn,
-      { [styles.primary]: buttonType === "primary",
-      [styles.dark]: buttonType === "dark",
-      [styles.lined]: buttonType === "bordered",
-      [styles.none]: buttonType === "none",
-    })} {...props}>{children}</button>;
+import styles from './button.module.scss';
+export function Button({
+  children,
+  buttonType = 'primary',
+  ...props
+}: ButtonProps): React.ReactElement {
+  return (
+    <button
+      className={cn(styles.btn, {
+        [styles.primary]: buttonType === 'primary',
+        [styles.dark]: buttonType === 'dark',
+        [styles.lined]: buttonType === 'bordered',
+        [styles.none]: buttonType === 'none',
+      })}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 }
